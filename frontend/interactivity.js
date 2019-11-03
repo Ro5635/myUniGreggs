bbe36034bbfdf9cf07afeb8be15d5c50e645d51d
Nat Baulch-Jones <nat.baulch-jones@skyscanner.net>
2019-11-03 11:34:48 +0000

    cheeky spaghetti JS because the deadline is close

--- frontend/interactivity.js
+++ frontend/interactivity.js
@@ -1,12 +1,19 @@
 $( document ).ready(function() {
-    initialParse();
+    $.getJSON( "../cleanup_data/high_value_data.json", function( json ) {
+        // dump json to leaderboard
+        const data = json;
+        const sortedData = data.sort((gregg1, gregg2) => gregg1.greggsDensity > gregg2.greggsDensity ? -1 : 1)
+        const leaderboard = document.getElementById("leaderboard");
+
+        let counter = 0;
+        sortedData.forEach(item => {
+
+            if(item.numStudents > 0){
+                counter++;
+                console.log(counter, item)
+
+                leaderboard.insertAdjacentHTML("beforeend", `<br>${counter} - <strong>${item.name}</strong> - Density of ${item.greggsDensity.toPrecision(4)}<br> with  ${item.numGreggsInViscinity} Greggs stores for ${item.numStudents} students<br/>`);
+            }
+        });
+    });
 });
-
-initialParse = () => {
-    $.getJSON( "universities-cleaned.json", function( json ) {
-        console.log( json );
-       });
-    
-    console.log(this.map);
-
-}
